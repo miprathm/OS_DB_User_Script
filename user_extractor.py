21c1ac8efbd20f0b7c38d048291f3f40ac5da206
unknown <user1@WDC-AUDIT-03.mcgm.gov.in>
2018-07-18 17:35:16 +0530

    CR:Prathm: DB script executed successfully

--- user_extractor.py
+++ user_extractor.py
@@ -2,6 +2,9 @@ import sys, os
 import re
 import openpyxl
 
+fill_color = openpyxl.styles.colors.Color(rgb='00ffff00')
+fill = openpyxl.styles.fills.PatternFill(patternType='solid', fgColor=fill_color)
+
 #Get path of the folder where output script is available
 pathname = os.path.abspath(sys.argv[0])
 if len(sys.argv) > 1 :
@@ -75,7 +78,13 @@ def get_ip_os_default_name(name):
 		ip = name
 	return ip;
 
-
+def get_ip_db_default_name(name):
+	ip_with_hostname_finder = re.compile('''
+			(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})?
+	''',re.X)
+	ip_with_hostname = ip_with_hostname_finder.search(name)
+	ip = ip_with_hostname.group(1)
+	return ip
 
 
 def extract_and_save_os_user(path):
@@ -84,18 +93,45 @@ def extract_and_save_os_user(path):
 	wb.save('OS.xlsx')
 	sheet = wb.get_active_sheet()
 	current_ip = 1
-	print('Path',path)	
+	print('--Path -> ',path)	
 	for dir in os.listdir(path):
-		print('dir',dir)
+		print('----Dir -> ',dir)
 		# for both DC/DR
 		for file in os.listdir(os.path.join(path,dir)):
-			print('file',file)
+			print('------file -> ',file)
 			ip = get_ip_or_default_name(file)
 			aix_06_block = fetch_AIX_06_block(os.path.join(path,dir,file))
 			usernames = fetch_os_users(aix_06_block)
+			sheet.cell(row=1,column=current_ip).fill = fill
 			sheet.cell(row=1,column=current_ip).value = ip
 			for users_index in range(2,len(usernames)):
 				sheet.cell(row=users_index,column=current_ip).value = usernames[users_index]
 			current_ip += 1
 	wb.save("OS.xlsx")		
-extract_and_save_os_user(pathname)			
+#extract_and_save_os_user(pathname)
+
+def extract_and_save_db_user(path):
+	wb = openpyxl.Workbook()
+	wb.save('DB.xlsx')
+	sheet = wb.get_active_sheet()
+	current_ip = 1
+	print('--Path',path)
+	for dcdrdir in os.listdir(path):
+		print('----DC/DR Dir -> ',dcdrdir)
+		for dir in os.listdir(os.path.join(path,dcdrdir)):
+			print('------Dir -> ',dir)
+			username = []
+			ip = dir#get_ip_db_default_name(dir)
+			for file in os.listdir(os.path.join(path,dcdrdir,dir)):
+				print("-------- File -> "file)
+				db_user_block = fetch_ORA_U11g_05_block(os.path.join(path,dcdrdir,dir,file))
+				username.append('')
+				username.append(file)
+				username.extend(fetch_db_user(db_user_block))
+			sheet.cell(row=1,column=current_ip).value = ip
+			sheet.cell(row=1,column=current_ip).fill = fill
+			for users_index in range(2,len(username)):
+				sheet.cell(row=users_index,column=current_ip).value = username[users_index]
+			current_ip += 1	
+		wb.save("DB.xlsx")	
+extract_and_save_db_user(pathname)				
