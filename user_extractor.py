937241294aec506037f1669673334f0e82749ca3
unknown <user1@WDC-AUDIT-03.mcgm.gov.in>
2018-07-18 15:47:27 +0530

    CR:Prathm: extract and save os user function implementd

--- user_extractor.py
+++ user_extractor.py
@@ -1,5 +1,6 @@
 import sys, os
 import re
+import openpyxl
 
 #Get path of the folder where output script is available
 pathname = os.path.abspath(sys.argv[0])
@@ -58,7 +59,43 @@ def fetch_db_user(string):
 	return usernames
 # ================================================== End of functions for DB Script ================================================== 
 
-ora_block = fetch_ORA_U11g_05_block(pathname)
-usernames = fetch_db_user(ora_block)
+#ora_11g_block = fetch_ORA_U11g_05_block(pathname)
+#usernames = fetch_db_user(ora_block)
 
-print(usernames)	
+#print(usernames)
+def get_ip_os_default_name(name):
+	ip_with_hostname_finder = re.compile('''
+			(.*)\_audit(\_(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))?
+	''',re.X)
+	ip_with_hostname = ip_with_hostname_finder.search(name)
+	#print(ip_with_hostname.groups())
+	hostname = ip_with_hostname.group(1)
+	ip = ip_with_hostname.group(3)
+	if ip is None:
+		ip = name
+	return ip;
+
+
+
+
+def extract_and_save_os_user(path):
+	# Create xlsx blank file
+	wb = openpyxl.Workbook()
+	wb.save('OS.xlsx')
+	sheet = wb.get_active_sheet()
+	current_ip = 1
+	print('Path',path)	
+	for dir in os.listdir(path):
+		print('dir',dir)
+		# for both DC/DR
+		for file in os.listdir(os.path.join(path,dir)):
+			print('file',file)
+			ip = get_ip_or_default_name(file)
+			aix_06_block = fetch_AIX_06_block(os.path.join(path,dir,file))
+			usernames = fetch_os_users(aix_06_block)
+			sheet.cell(row=1,column=current_ip).value = ip
+			for users_index in range(2,len(usernames)):
+				sheet.cell(row=users_index,column=current_ip).value = usernames[users_index]
+			current_ip += 1
+	wb.save("OS.xlsx")		
+extract_and_save_os_user(pathname)			
